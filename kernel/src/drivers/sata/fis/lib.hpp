468fbf88558287d33d5052a04110662fc78b1e81
ka <kaku.h9@gmail.com>
2025-10-19 14:53:13 +0100

    sata: lib.hpp fixed logic for extracting lba fields

--- kernel/src/drivers/sata/fis/lib.hpp
+++ kernel/src/drivers/sata/fis/lib.hpp
@@ -6,12 +6,15 @@
 // just a general library 
 
 namespace Drivers::Sata::Fis{
-  constexpr std::tuple<std::uint16_t, std::uint16_t, std::uint16_t> ExtractAddress(std::uint64_t address){
+  constexpr std::tuple<std::uint16_t, std::uint16_t, std::uint16_t, std::uint16_t, std::uint16_t, std::uint16_t> ExtractAddress(std::uint64_t address){
     // returns in order high, mid, low
     return std::make_tuple(
-      address &( 0xFFFFul << 32) >> 32,
-      address &( 0xFFFFul << 16) >> 16,
-      address & 0xFFFF
+        address & 0xFF,           // lba [7 : 0]
+        (address >> 8) & 0xFF,    // lba [15: 8]
+        (address >> 16) & 0xFF,   // lba [23: 16]
+        (address >> 24) & 0xFF,   // lba [31: 24]
+        (address >> 32) & 0xFF,   // lba [39: 32]
+        (address >> 40) & 0xFF   // lba [47: 40]
     );
   }
 }
