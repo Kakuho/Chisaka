d8ec37b661b8b93d3f8ffe16cda78e2650af86ca
ka <kaku.h9@gmail.com>
2026-01-16 22:36:29 +0000

    build_run_all_test.sh can handle hanging kernels now

--- kernel/integration_tests/build_run_all_test.sh
+++ kernel/integration_tests/build_run_all_test.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
+# still needs to add timeout for kernels that hang
+
+TESTS=("test2" "test1" "hanging")
 COUNT=0
+
+SUCCESS_TESTS=()
 SUCCESS_COUNT=0
+
+FAILED_TESTS=()
 FAILED_COUNT=0
 
-TESTS=("test2" "test1")
-FAILED_TESTS=()
-SUCCESS_TESTS=()
+HANGED_TESTS=()
+HANGED_COUNT=0
 
 for i in "${TESTS[@]}"
 do
   COUNT=$((COUNT+1))
   ./make_test.sh ${i}
-  ./run_test.sh ${i}
-  RET=$((($? & ~1) >> 1))
-  if [ $RET -eq 0 ]; then
-    SUCCESS_COUNT=$((${SUCCESS_COUNT} + 1))
-    SUCCESS_TESTS=("${SUCCESS_TESTS[@]}" "${i}")
+  timeout 10s ./run_test.sh ${i}
+  RET=$?
+  if [ $RET -eq 124 ]; then
+    HANGED_COUNT=$((${HANGED_COUNT} + 1))
+    HANGED_TESTS=("${HANGED_TESTS[@]}" "${i}")
   else
-    FAILED_COUNT=$((${FAILED_COUNT} + 1))
-    FAILED_TESTS=("${FAILED_TESTS[@]}" "${i}")
+    TEST_RET=$((($RET & ~1) >> 1))
+    if [ $TEST_RET -eq 0 ]; then
+      SUCCESS_COUNT=$((${SUCCESS_COUNT} + 1))
+      SUCCESS_TESTS=("${SUCCESS_TESTS[@]}" "${i}")
+    else
+      FAILED_COUNT=$((${FAILED_COUNT} + 1))
+      FAILED_TESTS=("${FAILED_TESTS[@]}" "${i}")
+    fi
   fi
 done
 
 echo "${SUCCESS_COUNT} / ${COUNT} Tests Succeded"
-echo "${SUCCESS_COUNT} / ${COUNT} Tests Failed"
+echo "${HANGED_COUNT} / ${COUNT} Tests Failed"
+echo "${FAILED_COUNT} / ${COUNT} Tests Failed"
 
 echo "Failed Tests:"
 
 for i in "${FAILED_TESTS[@]}"
+do
+  echo "  ${i}"
+done
+
+echo "Hanged Tests:"
+
+for i in "${HANGED_TESTS[@]}"
 do
   echo "  ${i}"
 done
---
+++ kernel/src/entry_points/hanging.cpp
@@ -0,0 +1,38 @@
+#include "drivers/serial/kostream.hpp"
+#include "firmware/limine/requests.hpp"
+#include "x86_64/utilites.hpp"
+#include "kassert.hpp"
+
+static constexpr std::uint8_t QEMU_SUCCESS = 0;
+static constexpr std::uint8_t QEMU_FAIL = 1;
+
+// Extern declarations for global constructor array.
+
+extern void (*__init_array[])();
+extern void (*__init_array_end[])();
+
+extern "C"{
+  void invokeinterrupt1();
+}
+
+extern "C"{
+  void outb(int port_addr, char val);
+}
+
+void ExitQemu(std::uint8_t code){
+  outb(0xf4, code);
+}
+
+static void Hanging(){
+  while(true){
+
+  }
+  ExitQemu(QEMU_SUCCESS);
+}
+
+extern "C" void _start() {
+  if(limine::is_limine_supported()){
+    X8664::HaltCatchFire();
+  }
+  Hanging();
+}
