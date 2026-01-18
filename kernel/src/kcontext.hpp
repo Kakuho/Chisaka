a57a1535a6cd2802b56392527d91c9c29b8a5279
ka <kaku.h9@gmail.com>
2026-01-18 21:12:38 +0000

    kcontext namespace added

---
+++ kernel/src/kcontext.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+// namespace to store and configure your global kernel managers
+
+#include "limine/utility.hpp"
+#include "types.hpp"
+
+namespace Chisaka::KContext{
+  static VirtAddr kernelBase;
+  static size_t hhdmOffset;
+
+  void InitKContext(){
+    kernelBase = Limine::kernel_addr_req.response->virtual_base;
+    hhdmOffset = Limine::hhdm_request.response->offset;
+  }
+
+  PhysAddr KernelPhysAddr(){
+    return kernelBase - hhdmOffset;
+  }
+
+  VirtAddr KernelVirtAddr(){
+    return kernelBase;
+  }
+
+  VirtAddr PhysToVirtAddr(PhysAddr paddr){
+    return paddr + hhdmOffset;
+  }
+
+  PhysAddr VirtToPhysAddr(VirtAddr vaddr){
+    return vaddr - hhdmOffset;
+  }
+}
