d3476346d212333e88c3e93b7fcf7887f8561770
ka <kaku.h9@gmail.com>
2026-01-18 22:12:16 +0000

    moved paging index parsing to x8664

--- kernel/src/x86_64/utilites.hpp
+++ kernel/src/x86_64/utilites.hpp
@@ -1,7 +1,9 @@
-#ifndef U_UTIL_HPP
-#define U_UTIL_HPP
+#pragma once
 
 // library for free standing cpu functions.
+
+#include "types.hpp"
+#include "drivers/serial/kostream.hpp"
 
 namespace X8664{
 
@@ -13,6 +15,19 @@ inline void HaltCatchFire(){
   }
 }
 
-}
+struct PageIndices{
+    std::uint16_t pm4le;
+    std::uint16_t pdpte;
+    std::uint16_t pde;
+    std::uint16_t pt;
+};
 
-#endif
+Chisaka::VirtAddr KVirtFromPageIndex(
+    PageIndices&& indices,
+    std::uint16_t offset
+);
+
+PageIndices ExtractPageIndicies(Chisaka::VirtAddr vaddr);
+void PrintPageIndicies(Chisaka::VirtAddr vaddr);
+
+}
