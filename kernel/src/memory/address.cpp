ede7d186917a7a790066c1e943ab2d3afb6ec29e
ka <kaku.h9@gmail.com>
2026-01-18 21:37:39 +0000

    removed redundant functions

--- kernel/src/memory/address.cpp
+++ kernel/src/memory/address.cpp
@@ -3,35 +3,6 @@
 namespace Mem{
 
 //-------------------------------------------------------------
-//  Useful Queries
-//-------------------------------------------------------------
-
-BaseLength LongestBaseLength(){
-  // setting up
-  if(Limine::memorymap_request.response == nullptr){
-    kout << "MEM FAIL\n";
-  }
-  limine_memmap_response* response = Limine::memorymap_request.response;
-  std::uint64_t totalEntries = response->entry_count;
-  limine_memmap_entry** entries = response->entries;
-  // now we can begin looping through to get largest available
-  // region
-  std::uint64_t largestIndex = 0;
-  for(std::uint64_t i = 0; i < totalEntries; i++){
-    // now set the responses
-    if(entries[i]->type == LIMINE_MEMMAP_USABLE){
-      if(entries[i]->length >= entries[largestIndex]->length){
-        largestIndex = i;
-      }
-    }
-  }
-  return BaseLength{
-    .base = entries[largestIndex]->base,
-    .length = entries[largestIndex]->length
-  };
-}
-
-//-------------------------------------------------------------
 //  Paging Address Related
 //-------------------------------------------------------------
 
