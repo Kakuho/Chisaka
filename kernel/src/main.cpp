7705610542a9b797edaafc60a02054a657fc9660
ka <kaku.h9@gmail.com>
2026-01-18 22:23:40 +0000

    updated main

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -1,21 +1,7 @@
 #include <cstdint>
 
-
 #include "limine/utility.hpp"
-#include "x86_64/timers/hpet_controller.hpp"
-
 #include "x86_64/utilites.hpp"
-#include "memory/heap/tests/test.hpp"
-#include "memory/heap/tests/page_size/test_run.hpp"
-
-#include "drivers/ahci/class_interface/samples/identify_device.hpp"
-#include "drivers/ahci/class_interface/samples/write_read.hpp"
-#include "drivers/ahci/class_interface/samples/change_addressable_sectors.hpp"
-
-#include "fs/merofs/samples/init.hpp"
-
-#include "x86_64/timers/samples/hpet_poll.hpp"
-#include "x86_64/timers/samples/tryhpet.hpp"
 
 // Extern declarations for global constructor array.
 
@@ -37,17 +23,6 @@ extern "C" void _start() {
       __init_array[i]();
   }
 
-  //X8664::Timers::Samples::TryHpet::TryOneShot();
-  //Drivers::Ahci::Class::Sample::WriteRead_AhciFuncs();
-  //Drivers::Ahci::Class::Sample::WriteRead_AhciFuncsMany();
-  //Drivers::Ahci::Class::Sample::WriteInterrupt();
-  //Drivers::Ahci::Class::Sample::IdentifyDevice_MemFun();
-  //Drivers::Ahci::Class::Sample::PrintMaxAddress();
-  Fs::Merofs::Samples::Initialise();
-  //Fs::Merofs::Samples::Hehe();
-  //Drivers::Ahci::Class::Sample::IdentifyDevice();
-  //Drivers::Ahci::Class::Sample::Write_OutOfBounds();
-
-  //kout << "Rarity is cute pony" << '\n';
+  kout << "Tottentemo Daisuke" << '\n';
   X8664::HaltCatchFire();
 }
