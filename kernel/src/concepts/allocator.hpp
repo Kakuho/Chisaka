c95fc3a5b4418a7c9de5b03da1ce9b4d265f73c7
ka <kaku.h9@gmail.com>
2026-01-19 21:13:54 +0000

    concepts: added HeapAllocator concept

--- kernel/src/concepts/allocator.hpp
+++ kernel/src/concepts/allocator.hpp
@@ -4,6 +4,7 @@
 #include <concepts>
 
 namespace Chisaka::Concepts{
+
   template<typename A>
   concept PageAllocator = requires(A a, unsigned pages, void* base){
     {a.AllocatePage()} -> std::same_as<void*>;
@@ -12,5 +13,11 @@ namespace Chisaka::Concepts{
   };
 
   template<typename A>
-  concept Allocator = PageAllocator<A>;
+  concept HeapAllocator = requires(A a, std::size_t nbytes, void* base){
+    {a.AllocateObject(nbytes)} -> std::same_as<void*>;
+    a.DeallocateObject(base);
+  };
+
+  template<typename A>
+  concept Allocator = PageAllocator<A> || HeapAllocator<A>;
 }
