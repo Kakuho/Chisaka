194ab81f8eee27ff84fdffcd723daf69a451f5ad
ka <kaku.h9@gmail.com>
2026-01-21 14:29:49 +0000

    slab: changed unit test compilation code

--- kernel/unit_tests/slab/compile_run.sh
+++ kernel/unit_tests/slab/compile_run.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
-g++ -std=c++20 -o main ./list_descriptor/linkage.cpp\
+SRC_DIR="/home/ka/C++/projects/os/Chisaka/git/slab_tests/kernel/src"
+CPPSRC="${SRC_DIR}/kheap/slab/list_descriptor.cpp"
+
+g++ -std=c++20 -D USERMODE_TESTING -o tests ./list_descriptor/linkage.cpp\
+      ${CPPSRC} \
       -I..  -I../../ext  -I../../src\
       ./../test_main.cpp 
+
+./tests
